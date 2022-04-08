f42c71426a25a869254fa1773e987bc0eee4a000
Nartlot <eduardo@foltran.sampa.br>
2022-04-08 16:25:07 -0300

    Mesuring NTC

--- Arduino/Code/Pet-o-nator/src/main.cpp
+++ Arduino/Code/Pet-o-nator/src/main.cpp
@@ -1,9 +1,46 @@
 #include <Arduino.h>
+#include <LiquidCrystal.h>
 
-void setup() {
-  // put your setup code here, to run once:
+#define NtcPin A0
+#define SenseResistor 5000.0
+
+const int rs = 12, en = 11, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
+LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
+
+void setup()
+{
+  // set up the LCD's number of columns and rows:
+  lcd.begin(16, 2);
+  // initialize the serial communications:
+  Serial.begin(9600);
+  Serial.write("Conectado\n");
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void loop()
+{
+  unsigned char i;
+  unsigned char j;
+  float avarege = 0;
+  unsigned int partAvarege;
+  float NtcResistance;
+  // when characters arrive over the serial port...
+  for (i = 0; i < 16;i++){
+    partAvarege = 0;
+    for (j = 0; j < 16;j++){
+      partAvarege += analogRead(NtcPin);
+      delay(2);
+    }
+    avarege += partAvarege / 16.0;
+  }
+  avarege /= 16.0;
+
+  NtcResistance = SenseResistor/1000.0 * (1024.0 - avarege) / avarege;
+  lcd.clear();
+  lcd.print(NtcResistance);
+  lcd.print("kOhms");
+  lcd.setCursor(0, 1);
+  // print the number of seconds since reset:
+  lcd.print(millis() / 1000);
+  
+  delay(100);
 }

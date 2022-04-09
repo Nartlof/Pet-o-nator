7722a937ac0047e8630c2a4dd6f4feda73cfbeb6
Nartlot <eduardo@foltran.sampa.br>
2022-04-09 17:07:46 -0300

    Testing the temperature control

--- Arduino/Code/Pet-o-nator/src/main.cpp
+++ Arduino/Code/Pet-o-nator/src/main.cpp
@@ -2,13 +2,15 @@
 #include <LiquidCrystal.h>
 
 #define NtcPin A0
-#define SenseResistor 5000.0
+#define SenseResistor 500.0
+#define PwmPin 6
 
 const int rs = 12, en = 11, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
 LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
 
 void setup()
 {
+  pinMode(PwmPin, OUTPUT);
   // set up the LCD's number of columns and rows:
   lcd.begin(16, 2);
   // initialize the serial communications:
@@ -23,24 +25,28 @@ void loop()
   float avarege = 0;
   unsigned int partAvarege;
   float NtcResistance;
-  // when characters arrive over the serial port...
-  for (i = 0; i < 16;i++){
+
+  analogWrite(PwmPin, analogRead(A1) / 4);
+
+  // Loading ADC for the NTC
+  for (i = 0; i < 16; i++)
+  {
     partAvarege = 0;
-    for (j = 0; j < 16;j++){
+    for (j = 0; j < 16; j++)
+    {
       partAvarege += analogRead(NtcPin);
-      delay(2);
     }
     avarege += partAvarege / 16.0;
   }
   avarege /= 16.0;
 
-  NtcResistance = SenseResistor/1000.0 * (1024.0 - avarege) / avarege;
+  NtcResistance = SenseResistor * (1024.0 - avarege) / avarege;
   lcd.clear();
   lcd.print(NtcResistance);
-  lcd.print("kOhms");
+  lcd.print(" Ohms");
   lcd.setCursor(0, 1);
   // print the number of seconds since reset:
   lcd.print(millis() / 1000);
-  
+
   delay(100);
 }
